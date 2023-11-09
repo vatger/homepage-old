c6e3e29b00efe925a30b3ce5fd030dc04a727ae5
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2023-11-09 12:01:51 +0100

    add warning

--- resources/views/membership/dashboard/warning.blade.php
+++ resources/views/membership/dashboard/warning.blade.php
@@ -1,5 +1,5 @@
 <section class="content-header">
     <div class="container-fluid">
-        <div class="row "> Wir stellen zum 12.11.2023 unsere Webseite. Um eine fehlerfreie Migration durchführen zu können sind einige Funktionen schon vorab deaktiviert! </div>
+        <div class="row "> Wir stellen zum 12.11.2023 unsere Webseite um. Um eine fehlerfreie Migration durchführen zu können sind einige Funktionen schon vorab deaktiviert! </div>
     </div>
 </section>
